3763fae7b5bfd20a9ed0670184a49d69ce18bd55
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2020-01-13 17:40:44 +0530

    ECOSYS-77 License added to source

--- src/main/UI.ts
+++ src/main/UI.ts
@@ -1,7 +1,9 @@
 'use strict';
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  https://github.com/microsoft/vscode-extension-samples/
  *--------------------------------------------------------------------------------------------*/
 
 import { QuickPickItem, window, Disposable, CancellationToken, QuickInputButton, QuickInput, ExtensionContext, QuickInputButtons, Uri } from 'vscode';
