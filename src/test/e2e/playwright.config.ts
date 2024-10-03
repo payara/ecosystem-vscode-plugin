fa80229d98b785656a065656552d6d59d80f7f51
Simon Laden <simon.laden@payara.fish>
2024-10-03 17:02:31 +0100

    FISH-8908 - add e2e tests

---
+++ src/test/e2e/playwright.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from '@playwright/test';
+import type { TestOptions } from './baseTest.ts';
+
+// eslint-disable-next-line import-x/no-default-export
+export default defineConfig<TestOptions>({
+	use: {
+		headless: true, // Ensure headless mode is enabled
+		viewport: { width: 1920, height: 1080 },
+	},
+	reporter: 'list', // process.env.CI ? 'html' : 'list',
+	timeout: 60000, // 1 minute
+	workers: 2,
+	expect: {
+		timeout: 60000, // 1 minute
+	},
+	globalSetup: './setup',
+	outputDir: '../../../out/test-results',
+	projects: [
+		{
+			name: 'VSCode stable',
+			use: {
+				vscodeVersion: 'stable',
+			},
+		},
+		{
+			name: 'VSCode insiders',
+			use: {
+				vscodeVersion: 'insiders',
+			},
+		},
+	],
+	testMatch: '**/specs/*.test.ts'
+});
